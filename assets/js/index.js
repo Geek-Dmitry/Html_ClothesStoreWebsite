33f71c7834f030c036494eebeffcc536a49bf5e5
Geek-Dmitry <salata.dzmitriy90@gmail.com>
2021-09-03 19:40:54 +0300

    -animation while scrolling added
    -new function animOnScroll added;
    -new classes in the Style.css added;

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    //activate item in the header menu while scrolling
     var sections = $('section')
         , nav = $('nav')
         , nav_height = nav.outerHeight();
@@ -47,4 +47,36 @@ $(document).ready(function () {
         $('body,html').animate({ scrollTop: top }, 1500);
     });
 
+    const animItems = document.querySelectorAll(".scroll-animation");
+
+    if (animItems.length > 0) {
+        window.addEventListener("scroll", animOnScroll)
+
+        function animOnScroll(params) {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffSet = offset (animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+                if(animItemHeight > window.innerHeight) {
+                    let animItemPoint = window.innerHeight -  window.innerHeight / animStart;
+                }
+
+                if((scrollY > animItemOffSet - animItemPoint) && scrollY < (animItemOffSet + animItemHeight)) {
+                    animItem.classList.add("active");
+                }
+            }
+        }
+
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+            scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+            scrollTop = window.screenY || document.documentElement.scrollTop;
+            return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+    }
+    
+    animOnScroll();
 });
